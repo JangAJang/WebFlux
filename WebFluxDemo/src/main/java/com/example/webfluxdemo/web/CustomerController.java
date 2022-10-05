96532a086819112337ace6a758e41ae6f19c82c8
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-10-05 20:48:39 +0900

    feat: add Flux and FluxStream function

--- WebFluxDemo/src/main/java/com/example/webfluxdemo/web/CustomerController.java
+++ WebFluxDemo/src/main/java/com/example/webfluxdemo/web/CustomerController.java
@@ -3,15 +3,30 @@ package com.example.webfluxdemo.web;
 import com.example.webfluxdemo.domain.Customer;
 import com.example.webfluxdemo.domain.CustomerRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+
+import java.time.Duration;
 
 @RestController
 @RequiredArgsConstructor
 public class CustomerController {
 
     private final CustomerRepository customerRepository;
+
+    //각 엘레멘트당 딜레이 1초씩 해서 로그는 1초마다 올라오지만 웹상으로는 총 딜레이 5초 이후에 한번에 json타입으로 결과가 나온다.
+    @GetMapping("/flux")
+    public Flux<Integer> flux(){
+        return Flux.just(1, 2, 3, 4, 5).delayElements(Duration.ofSeconds(1)).log();
+    }
+
+    //각 엘레멘트당 딜레이 1초씩 로그와 웹 결과가 나온다. 
+    @GetMapping(value = "/fluxstream", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+    public Flux<Integer> fluxstream(){
+        return Flux.just(1, 2, 3, 4, 5).delayElements(Duration.ofSeconds(1)).log();
+    }
 
     @GetMapping("/customer")
     public Flux<Customer> findAll(){
