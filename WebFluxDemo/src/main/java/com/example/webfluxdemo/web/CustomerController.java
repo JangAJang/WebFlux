4583e46f0e234bc8b9f08c53c2262f96f4e3eab2
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-10-05 21:06:05 +0900

    feat: add FindByid function

--- WebFluxDemo/src/main/java/com/example/webfluxdemo/web/CustomerController.java
+++ WebFluxDemo/src/main/java/com/example/webfluxdemo/web/CustomerController.java
@@ -5,8 +5,10 @@ import com.example.webfluxdemo.domain.CustomerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 
@@ -22,14 +24,21 @@ public class CustomerController {
         return Flux.just(1, 2, 3, 4, 5).delayElements(Duration.ofSeconds(1)).log();
     }
 
-    //각 엘레멘트당 딜레이 1초씩 로그와 웹 결과가 나온다. 
+    //각 엘레멘트당 딜레이 1초씩 로그와 웹 결과가 나온다.
     @GetMapping(value = "/fluxstream", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
     public Flux<Integer> fluxstream(){
         return Flux.just(1, 2, 3, 4, 5).delayElements(Duration.ofSeconds(1)).log();
     }
 
+    //여러 건 조회할 땐 Flux
     @GetMapping("/customer")
     public Flux<Customer> findAll(){
         return customerRepository.findAll().log();
+    }
+
+    //단건 조회할 땐 Mono
+    @GetMapping("/customer/{id}")
+    public Mono<Customer> findById(@PathVariable("id") Long id){
+        return customerRepository.findById(id);
     }
 }
