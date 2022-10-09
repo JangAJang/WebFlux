bc8a2bf39109a026fba091ae9ba130f365c44d12
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-10-09 18:26:02 +0900

    refactor: extract common part

--- chat_front/js/chat.js
+++ chat_front/js/chat.js
@@ -3,34 +3,48 @@ eventSource.onmessage =( event) =>{
     console.log(1, event);
     const data = JSON.parse(event.data);
     console.log(2, data);
+    initSendMessage(data);
 }
 
-function getSendMessageBox(msg){
+function getSendMessageBox(msg, time){
     return `<div id="chat-sent-msg" class="sent_msg">
     <p>${msg}</p>
-    <span class="time_date"> 11:18 | Today</span>
+    <span class="time_date"> ${time}</span>
     </div>`;
+}
+
+function addSendMessage(){
+    let chatBox = document.querySelector("#chat-box");
+    let msgInputBox = document.querySelector("#chat-outgoing-msg");
+    let chatOutgoingBox = document.createElement("div");
+    let date = new Date();
+    let now = date.getHours() + ":" + date.getMinutes() + " | Today";
+    chatOutgoingBox.className = "outgoing_msg"
+    chatOutgoingBox.innerHTML = getSendMessageBox(msgInputBox.value, now);
+    chatBox.append(chatOutgoingBox);
+    msgInputBox.value = "";
+}
+
+function initSendMessage(data){
+    let chatBox = document.querySelector("#chat-box");
+    let msgInputBox = document.querySelector("#chat-outgoing-msg");
+    let chatOutgoingBox = document.createElement("div");
+    chatOutgoingBox.className = "outgoing_msg";
+    let md = data.createdAt.substring(5,10);
+    let tm = data.createdAt.substring(11,16);
+    convertTime = tm + " | " + md;
+    chatOutgoingBox.innerHTML = getSendMessageBox(data.msg, convertTime);
+    chatBox.append(chatOutgoingBox);
+    msgInputBox.value = "";
 }
 
 
 document.querySelector("#chat-send").addEventListener("click", ()=>{
-    let chatBox = document.querySelector("#chat-box");
-    let msgInputBox = document.querySelector("#chat-outgoing-msg");
-    let chatOutgoingBox = document.createElement("div");
-    chatOutgoingBox.className = "outgoing_msg"
-    chatOutgoingBox.innerHTML = getSendMessageBox(msgInputBox.value);
-    chatBox.append(chatOutgoingBox);
-    msgInputBox.value = "";
+    addSendMessage();
 });
 
 document.querySelector("#chat-outgoing-msg").addEventListener("keydown", (e)=>{
     if(e.keyCode === 13){
-        let chatBox = document.querySelector("#chat-box");
-        let msgInputBox = document.querySelector("#chat-outgoing-msg");
-        let chatOutgoingBox = document.createElement("div");
-        chatOutgoingBox.className = "outgoing_msg"
-        chatOutgoingBox.innerHTML = getSendMessageBox(msgInputBox.value);
-        chatBox.append(chatOutgoingBox);
-        msgInputBox.value = "";
+        addSendMessage();
     }
 });
