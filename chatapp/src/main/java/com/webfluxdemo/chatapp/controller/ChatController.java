5d95f7a9dc9e5250556c3acdca032b121bc8de8d
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-10-09 16:17:51 +0900

    feat: add ChatController

---
+++ chatapp/src/main/java/com/webfluxdemo/chatapp/controller/ChatController.java
@@ -0,0 +1,31 @@
+package com.webfluxdemo.chatapp.controller;
+
+import com.webfluxdemo.chatapp.Entity.Chat;
+import com.webfluxdemo.chatapp.repository.ChatRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequiredArgsConstructor
+public class ChatController {
+
+    private final ChatRepository chatRepository;
+
+    @GetMapping(value = "/find", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Chat> getMsg(@RequestParam("sender") String sender, @RequestParam("receiver")String receiver){
+        return chatRepository.findBySenderAndReceiver(sender, receiver)
+                .subscribeOn(Schedulers.boundedElastic());
+    }
+
+    @PostMapping("/chat")
+    public Mono<Chat> setMsg(@RequestBody Chat chat){
+        chat.setCreatedAt(LocalDateTime.now());
+        return chatRepository.save(chat);
+    }
+}
